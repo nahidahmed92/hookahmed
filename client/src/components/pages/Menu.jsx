d023b8f4286f1846179d058f1411e569236e08fd
Nahid Ahmed <npahmed92@gmail.com>
2024-10-20 17:41:32 -0400

    create a menu page

---
+++ client/src/components/pages/Menu.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+
+export default function Menu() {
+  const navigate = useNavigate();
+
+  const handleHookahBtn = () => {
+    navigate('/menu/flavor');
+  };
+
+  const handleDrinksBtn = () => {
+    navigate('/menu/drinks');
+  };
+
+  return (
+    <>
+      <div className="d-flex flex-column align-items-center">
+        <h1>Menu</h1>
+        <button className="btn btn-primary mt-5 w-25" onClick={handleHookahBtn}>
+          Hookahs
+        </button>
+        <button className="btn btn-primary mt-5 w-25" onClick={handleDrinksBtn}>
+          Drinks
+        </button>
+      </div>
+    </>
+  );
+}
