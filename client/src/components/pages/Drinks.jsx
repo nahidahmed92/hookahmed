0c63805e0423e4c5711968fd3fc0a18ad21de1ee
Nahid Ahmed <npahmed92@gmail.com>
2024-10-23 14:18:17 -0400

    create a map from drinks array to avoid repetition

--- client/src/components/pages/Drinks.jsx
+++ client/src/components/pages/Drinks.jsx
@@ -7,14 +7,20 @@ export default function Drinks() {
   const navigate = useNavigate();
   const { addToCart } = useCart();
 
+  // Array of drinks objects
+  const drinks = [
+    { name: 'Diet Coke', description: 'This is where the drink description will go.' },
+    { name: 'Coke', description: 'This is where the drink description will go.' },
+  ];
+
   const handleAddBtn = (drinkName) => {
-    // TODO: implement add button functionality here
     const item = {
       name: drinkName,
       type: 'Drink',
       quantity: 1, // Adding as a single item
     };
     addToCart(item);
+    // TODO: open cart after an item has been added
   };
 
   const handlePrevBtn = () => {
@@ -26,50 +32,30 @@ export default function Drinks() {
       <h1>Drinks Menu</h1>
       <p>Drinks are free I guess.</p>
       <div className="d-flex flex-column align-items-center">
-        <div className="card mb-3" style={{ maxWidth: '540px' }}>
-          <div className="row g-0">
-            <div className="col-md-4">
-              <img src={logo} className="img-fluid rounded-start" alt="..." />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h5 className="card-title d-flex justify-content-start align-items-left">
-                  Diet Coke
-                </h5>
-                <p className="card-text d-flex justify-content-start">
-                  This is where the flavor profile will go.
-                </p>
+        {drinks.map((drink) => (
+          <div className="card mb-3" style={{ maxWidth: '540px' }} key={drink.name}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={logo} className="img-fluid rounded-start" alt={drink.name} />
               </div>
-              <div className="d-flex flex-column align-items-end mx-2">
-                <button
-                  className="btn btn-primary mx-2 mt-2"
-                  onClick={() => handleAddBtn('Diet Coke')}>
-                  Add
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card mb-3" style={{ maxWidth: '540px' }}>
-          <div className="row g-0">
-            <div className="col-md-4">
-              <img src={logo} className="img-fluid rounded-start" alt="..." />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h5 className="card-title d-flex justify-content-start align-items-left">Coke</h5>
-                <p className="card-text d-flex justify-content-start">
-                  This is where the flavor profile will go.
-                </p>
-              </div>
-              <div className="d-flex flex-column align-items-end mx-2">
-                <button className="btn btn-primary mx-2 mt-2" onClick={() => handleAddBtn('Coke')}>
-                  Add
-                </button>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title d-flex justify-content-start align-items-left">
+                    {drink.name}
+                  </h5>
+                  <p className="card-text d-flex justify-content-start">{drink.description}</p>
+                </div>
+                <div className="d-flex flex-column align-items-end mx-2">
+                  <button
+                    className="btn btn-primary mx-2 mt-2"
+                    onClick={() => handleAddBtn(drink.name)}>
+                    Add
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
       <div className="mt-3">
         <button className="btn btn-primary mx-2" onClick={handlePrevBtn}>
