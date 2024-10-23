7b26c83e8221bd3c16f8cc99a0ac1d240c0c08be
Nahid Ahmed <npahmed92@gmail.com>
2024-10-23 13:32:22 -0400

    create a map from bases array to avoid repetition

--- client/src/components/pages/Base.jsx
+++ client/src/components/pages/Base.jsx
@@ -28,46 +28,28 @@ export default function Base() {
       <h1>Bases</h1>
       <p>Select a base of your choice.</p>
       <div className="d-flex flex-column align-items-center">
-        <div className="card mb-3" style={{ maxWidth: '540px' }}>
-          <div className="row g-0">
-            <div className="col-md-4">
-              <img src={logo} className="img-fluid rounded-start" alt="..." />
-            </div>
-            <div className="col-md-8 d-flex flex-column">
-              <div className="card-body">
-                <h5 className="card-title d-flex justify-content-start align-items-left">Smoke</h5>
-                <p className="card-text d-flex justify-content-start">
-                  This is where the flavor profile will go.
-                </p>
+        {bases.map((base) => (
+          <div className="card mb-3" style={{ maxWidth: '540px' }} key={base.name}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={logo} className="img-fluid rounded-start" alt={base.name} />
               </div>
-              <div className="d-flex flex-column align-items-end mx-2 mb-2">
-                <button className="btn btn-primary" onClick={handleAddBtn}>
-                  Add
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card mb-3" style={{ maxWidth: '540px' }}>
-          <div className="row g-0">
-            <div className="col-md-4">
-              <img src={logo} className="img-fluid rounded-start" alt="..." />
-            </div>
-            <div className="col-md-8 d-flex flex-column">
-              <div className="card-body">
-                <h5 className="card-title d-flex justify-content-start align-items-left">Clear</h5>
-                <p className="card-text d-flex justify-content-start">
-                  This is where the flavor profile will go.
-                </p>
-              </div>
-              <div className="d-flex flex-column align-items-end mx-2 mb-2">
-                <button className="btn btn-primary" onClick={handleAddBtn}>
-                  Add
-                </button>
+              <div className="col-md-8 d-flex flex-column">
+                <div className="card-body">
+                  <h5 className="card-title d-flex justify-content-start align-items-left">
+                    {base.name}
+                  </h5>
+                  <p className="card-text d-flex justify-content-start">{base.description}</p>
+                </div>
+                <div className="d-flex flex-column align-items-end mx-2 mb-2">
+                  <button className="btn btn-primary" onClick={handleAddBtn}>
+                    Add
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
       <div className="mt-3">
         <button className="btn btn-primary mx-2" onClick={handlePrevBtn}>
