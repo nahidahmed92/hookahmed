83715bb9ba257d3fbeb9f1fffec82f92fb0ab86c
Nahid Ahmed <npahmed92@gmail.com>
2024-10-23 20:57:11 -0400

    remove empty fragment

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,33 +3,31 @@ import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ borderRadius: '8px' }}>
-        <div className="container-fluid">
-          <div className="d-flex">
-            <a className="navbar-brand" href="/">
-              <img src={logo} alt="logo" width="50px" />
-            </a>
-            <div className="navbar" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/menu">
-                    Menu
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
+    <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ borderRadius: '8px' }}>
+      <div className="container-fluid">
+        <div className="d-flex">
+          <a className="navbar-brand" href="/">
+            <img src={logo} alt="logo" width="50px" />
+          </a>
           <div className="navbar" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-            <a className="nav-link text-black mx-2" href="#">
-              Login
-            </a>
-            <Cart />
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/menu">
+                  Menu
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </nav>
-    </>
+
+        <div className="navbar" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+          <a className="nav-link text-black mx-2" href="#">
+            Login
+          </a>
+          <Cart />
+        </div>
+      </div>
+    </nav>
   );
 }
